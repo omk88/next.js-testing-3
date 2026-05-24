7b5e14e3844aaa77f7b8940967695d717b564b00
Emmanuel Learmount <oldmanjenkins56@gmail.com>
2026-05-24 13:20:14 +0100

    Search function functionality

--- components/web/SearchInput.tsx
+++ components/web/SearchInput.tsx
@@ -1,14 +1,52 @@
-import { Search } from "lucide-react";
+import { Loader2, Search } from "lucide-react";
 import { Input } from "../ui/input";
+import { useState } from "react";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import Link from "next/link";
 
 export function SearchInput() {
+
+    const [term, setTerm] = useState("");
+    const [open, setOpen] = useState(false);
+
+    const results = useQuery(api.posts.searchPosts, term.length >= 2 ? { limit: 5, term: term }: "skip");
+
+    function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setTerm(e.target.value);
+        setOpen(true);
+    }
+
     return (
         <div className="relative w-full max-w-sm">
             <div className="relative">
                 <Search className="absolute left-2.5 top-2.5 size-4 text-muted-foreground" />
 
-                <Input className="w-full pl-8 bg-background" type="search" placeholder="Search Posts..." />
+                <Input className="w-full pl-8 bg-background" type="search" placeholder="Search Posts..." value={term} onChange={handleInputChange}/>
             </div>
+
+            { open && term.length >= 2 && (
+                <div className="absolute top-full mt-2 rounded-md border bg-popover text-popover-foreground shadow-md outline-none animate-in fade-in-0 zoom-in-95">
+                    { results === undefined ? (
+                        <div>
+                            <Loader2 />
+                            Searching...
+                        </div>
+                    ): results.length === 0 ? (
+                        <p>
+                            No results found!
+                        </p>
+                    ): (
+                        <div className="py-1">
+                            { results.map((post) => (
+                                <Link href={`/blog/${post._id}`} key={post._id}>
+                                    <p>{ post.title }</p>
+                                </Link>
+                            )) }
+                        </div>
+                    ) }
+                </div>
+            ) }
         </div>
     );
 }
